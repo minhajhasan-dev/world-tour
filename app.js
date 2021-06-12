df623f4cc142bbd4d80c0b42fc8a474f5c0714df
minhajhasan-dev <minhajhasan.dev@gmail.com>
2021-06-13 01:49:08 +0600

    this is a message forEach

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res => res.json())
+.then(data => displayCountries(data));
+
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
+        const countryInfo = `
+            <h3 class='country-name'> ${country.name} </h3>
+            <p> ${country.capital} </p>
+        `;
+        countryDiv.innerHTML = countryInfo;
+        countriesDiv.appendChild(countryDiv);
+    });
+
+    // for (let i = 0; i < countries.length; i++) {
+        
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement('div');
+    //     countryDiv.className = 'country';
+    //     const countryInfo = `
+    //         <h3 class='country-name'> ${country.name} </h3>
+    //         <p> ${country.capital} </p>
+    //     `;
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
+    // }
+}
